b3b201a7bae5479f585896e7cb93f1d506978bc4
atmashere <atmashere@ya.ru>
2017-12-26 21:13:32 +0300

    Output correction

--- main.cpp
+++ main.cpp
@@ -18,47 +18,58 @@
 
 //#include "array.h"
 
+void showResult(TModel* model);
+
 int main()
 {
 
 
-TModel* Model = new TArenstorfModel2();
-
-TIntegrator* Integrator = new TDormandPrinceIntegrator();
-	Integrator->setPrecision(1e-16);
-	printf ("1");
-	Integrator->Run( Model );
-	printf ("2");
-	TMatrix Result = Model->getResult();
-
-	TVector Res = Model->getInitialConditions();
-	
-	
-    printf ("\nTVector Res\n");
-	for (int z=0; z<4; z++)
-	{
-		printf("%f   ", Res[z]);
-	}
-	
-	
-	//printf("Model = %5.2f   ", Model->getInitialConditions());
-	
-	printf("\nCount string Matrix_x = %i", Result.rowCount());
-	printf("\nCount column Matrix_x = %i", Result.colCount());
-	
-
-    printf("\nResult: \n");
-	for (int i=0; i<Result.rowCount(); i++)
-	{
-		for (int j=0; j<Result.colCount(); j++)
-		{
-            printf("%5.3f;", Result(i,j));
-		}
-		printf("\n");
-	}
-	
-
-	delete Integrator;
+    TModel* Model = new TArenstorfModel();
+    showResult(Model);
 	delete Model;
+
+    TModel* Model2 = new TArenstorfModel2();
+    showResult(Model2);
+    delete Model2;
+
 	return 0;
+}
+
+
+void showResult(TModel* model){
+
+    TIntegrator* Integrator = new TDormandPrinceIntegrator();
+        Integrator->setPrecision(1e-16);
+        printf ("1");
+        Integrator->Run( model );
+        printf ("2");
+        TMatrix Result = model->getResult();
+
+        TVector Res = model->getInitialConditions();
+
+
+        printf ("\nTVector Res\n");
+        for (int z=0; z<4; z++)
+        {
+            printf("%f   ", Res[z]);
+        }
+
+
+
+        printf("\nCount string Matrix_x = %i", Result.rowCount());
+        printf("\nCount column Matrix_x = %i", Result.colCount());
+
+
+        printf("\nResult: \n");
+        for (int i=0; i<Result.rowCount(); i++)
+        {
+            for (int j=0; j<Result.colCount(); j++)
+            {
+                printf("%5.7f ; ", Result(i,j));
+            }
+            printf("\n");
+        }
+
+
+        delete Integrator;
 }
